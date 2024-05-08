61b43f1a56d41451fd800216c9a1d386905a8cba
DTEMBEKTAS <emirhan.bektas2@ext.digiturk.com.tr>
2024-05-08 09:32:26 +0300

    .

---
+++ app/containers/Navbar/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Navbar() {
+  return <div>ARSLAN ZIRAAT</div>;
+}
+
+export default Navbar;
---
+++ app/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../../containers/Navbar";
+import Hero from "../../containers/Hero";
+
+function HomePage() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default HomePage;
